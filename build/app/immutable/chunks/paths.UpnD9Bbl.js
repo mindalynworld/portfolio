322b4206506da4f2c2f82195f57a95a86a0ed6cc
Mindy <mindylynnng@gmail.com>
2024-12-12 14:07:01 -0800

    config and export updates

---
+++ build/app/immutable/chunks/paths.UpnD9Bbl.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1ilxzlm)==null?void 0:s.base)??"/portfolio/build";var l;const a=((l=globalThis.__sveltekit_1ilxzlm)==null?void 0:l.assets)??t;export{a,t as b};
--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,8 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH ///portfolio
+            //base: process.argv.includes('dev') ? '' : process.env.BASE_PATH ///portfolio
+			base: process.env.NODE_ENV === 'production' ? '/portfolio' : '', // process.env.BASE_PATH ///portfolio
         }
 	}
 };
