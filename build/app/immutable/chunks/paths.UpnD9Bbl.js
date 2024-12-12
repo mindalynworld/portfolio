148e9023856dfb7a8461adaa93d6f74f29782d39
Mindy <mindylynnng@gmail.com>
2024-12-12 14:53:11 -0800

    delete build folder

--- build/app/immutable/chunks/paths.UpnD9Bbl.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1ilxzlm)==null?void 0:s.base)??"/portfolio/build";var l;const a=((l=globalThis.__sveltekit_1ilxzlm)==null?void 0:l.assets)??t;export{a,t as b};
